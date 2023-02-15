998e256dc8373aa9b51a6abdd2cf5dbf7a0b320a
Edwinndungu <endungu361@gmail.com>
2023-02-15 14:18:32 +0300

    dart basic introductory

---
+++ index.dart
@@ -0,0 +1,23 @@
+// Declare an integer variable
+int age = 22;
+
+// Declare a double variable
+double height = 5.8;
+
+// Declare a string variable
+String name = 'EDWIN';
+
+// Declare a list of integers
+List<String> fruits = ["mango","apple","kiwi"];
+
+// Declare a map of strings to integers
+Map<String, int> phoneNumbers = {
+  'John': 254712345456,
+  'Jane': 254709934434,
+};
+
+void main() {
+  print('My name is $name, I am $age years old and $height meters tall.');
+  print('My favorite fruits are: $fruits');
+  print('My friends\' phone numbers are: $phoneNumbers');
+}
